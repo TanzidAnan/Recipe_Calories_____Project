285f03a432f692f4c278d172b8d39c7bdb300bac
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-23 10:39:16 +0600

    card section done

--- src/Compontent/Recipes/Recipes.jsx
+++ src/Compontent/Recipes/Recipes.jsx
@@ -3,15 +3,40 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const Recipes = () => {
-    const [recipes,setRecipes] =useState([]);
-    useEffect(() =>{
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
         fetch('Food.json')
-        .then(res => res.json())
-        .then(data => setRecipes(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setRecipes(data))
+    }, [])
     return (
-        <div className="w-2/3">
-           <h1>Recipes {recipes.length}</h1> 
+        <div className="w-2/3 mt-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                {
+                    recipes.map(recipe => <div key={recipe.recipe_id}>
+                        <div className="card bg-white text-black w-96 shadow-xl ">
+                            <figure className="px-8 py-6">
+                                <img className="md:h-60 w-full rounded-xl"
+                                    src={recipe.recipe_image}
+                                    alt="recipe img" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title text-xl text-slate-600 font-medium">{recipe.recipe_name}</h2>
+                                <p className="text-gray-600 text-base font-semibold">{recipe.short_description}</p>
+                                <h2 className="text-xl text-slate-700 font-medium">Ingredients: {recipe.ingredients.length}</h2>
+                                <ul className="ml-8">
+                                    {
+                                        recipe.ingredients.map((item, idx) =><li className="list-disc text-gray-700" key={idx}>{item}</li>)
+                                    }
+                                </ul>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
