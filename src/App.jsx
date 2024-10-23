df356be4d589d9c876c072d213fe578881e84c06
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-23 15:59:28 +0600

    calcultater

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,48 @@ import Recipes from './Compontent/Recipes/Recipes'
 import Sitbar from './Compontent/Sitbar/Sitbar'
 
 function App() {
-  const [recipesQueue,setRecipesQueue] =useState([]);
-  const [preperRecipe,setPrePerRecipe] =useState([])
-  const addRecipesToQueue =(recipe) =>{
-    const isExiten =recipesQueue.find(priviousRecipes => priviousRecipes.recipe_id ===recipe.recipe_id)
-    if(!isExiten){
-      setRecipesQueue([...recipesQueue,recipe])
+  const [recipesQueue, setRecipesQueue] = useState([]);
+  const [preperRecipe, setPrePerRecipe] = useState([]);
+  const [totleTime, setTotleTime] = useState(0);
+  const [totleCalure, setTotleCalure] = useState(0);
+  const addRecipesToQueue = (recipe) => {
+    const isExiten = recipesQueue.find(priviousRecipes => priviousRecipes.recipe_id === recipe.recipe_id)
+    if (!isExiten) {
+      setRecipesQueue([...recipesQueue, recipe])
     }
-    else{
+    else {
       alert('Add a cart')
     }
-    
+
   }
-  const recipeRemove = id =>{
-    const deleteRecipes =recipesQueue.find(recipe => recipe.recipe_id ===id);
+  const recipeRemove = id => {
+    const deleteRecipes = recipesQueue.find(recipe => recipe.recipe_id === id);
 
     const updetsQueue = recipesQueue.filter(recipe => recipe.recipe_id !== id);
     setRecipesQueue(updetsQueue);
-    setPrePerRecipe([...preperRecipe,deleteRecipes])
+    setPrePerRecipe([...preperRecipe, deleteRecipes])
 
+  }
+  const calculaterTimeAndCalories = (time, calorie) => {
+    setTotleTime(totleTime + time);
+    setTotleCalure(totleCalure + calorie)
   }
 
   return (
     <>
-    <div className='w-10/12 mx-auto'>
-    <Navber></Navber>
-    <Banner></Banner>
-      <section className='flex flex-col lg:flex-row gap-6'>
-        <Recipes addRecipesToQueue={addRecipesToQueue}></Recipes>
+      <div className='w-10/12 mx-auto'>
+        <Navber></Navber>
+        <Banner></Banner>
+        <section className='flex flex-col lg:flex-row gap-6'>
+          <Recipes addRecipesToQueue={addRecipesToQueue}></Recipes>
 
-        <Sitbar recipeRemove={recipeRemove} recipesQueue={recipesQueue}></Sitbar>
-      </section>
-    </div>
+          <Sitbar recipeRemove={recipeRemove} recipesQueue={recipesQueue} preperRecipe={preperRecipe}
+          calculaterTimeAndCalories={calculaterTimeAndCalories}
+          totleTime={totleTime}
+          totleCalure={totleCalure}
+          ></Sitbar>
+        </section>
+      </div>
     </>
   )
 }
--- src/Compontent/Sitbar/Sitbar.jsx
+++ src/Compontent/Sitbar/Sitbar.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 
-const Sitbar = ({ recipesQueue,recipeRemove }) => {
+const Sitbar = ({ recipesQueue, recipeRemove, preperRecipe,calculaterTimeAndCalories,totleTime,totleCalure }) => {
     return (
         <div className="w-full lg:w-1/3 mt-4 border-2 rounded-2xl bg-white">
             <h1 className="text-center text-2xl font-bold mt-2">Went To cook : {recipesQueue.length}</h1>
@@ -25,9 +26,47 @@ const Sitbar = ({ recipesQueue,recipeRemove }) => {
                                         <td>{recipe.recipe_name}</td>
                                         <td>{recipe.preparing_time}</td>
                                         <td>{recipe.calories}</td>
-                                        <td><button onClick={() => recipeRemove(recipe.recipe_id)} className="btn bg-green-500 hover:bg-green-500 text-white rounded-2xl border-none">Priping</button></td>
+                                        <td><button onClick={() => {
+                                            recipeRemove(recipe.recipe_id)
+                                            calculaterTimeAndCalories(recipe.preparing_time,recipe.calories)
+                                        }} className="btn bg-green-500 hover:bg-green-500 text-white rounded-2xl border-none">Priping</button></td>
                                     </tr>)
                             }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div>
+                <h1 className="text-center text-2xl font-bold mt-2">current cooking : {preperRecipe.length}</h1>
+                <div className="overflow-x-auto text-black font-medium">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Time</th>
+                                <th>Calories</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                preperRecipe.map((recipe, idx) =>
+                                    <tr className="hover hover:text-white" key={idx}>
+                                        <th>{idx + 1}</th>
+                                        <td>{recipe.recipe_name}</td>
+                                        <td>{recipe.preparing_time}</td>
+                                        <td>{recipe.calories}</td>
+                                    </tr>)
+                            }
+                            <tr className="border-none">
+                                <th></th>
+                                <td></td>
+                                <td>Totle Time= {totleTime}</td>
+                                <td>Totle calories= {totleCalure}</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
